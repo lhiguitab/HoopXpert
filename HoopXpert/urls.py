f90a485e7e862c65721cdf3b61c14fbb983d691a
lhiguitab <lhiguitab@eafit.edu.co>
2025-02-19 14:25:11 -0500

    User App Changed

--- HoopXpert/urls.py
+++ HoopXpert/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from User import views as user_views
+from user import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
